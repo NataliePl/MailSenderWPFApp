9be6dee113d876041ebd913114b48ef03e586c48
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-02-15 13:17:17 +0300

    Логика отправки сообщений smtp

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace TestConsole
 {
@@ -6,7 +8,24 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var from = new MailAddress("n.plaksickaya@yandex.ru", "From_Наталия");
+            var to = new MailAddress("n.plaksickaya@gmail.com", "To_Наталия");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "Тестовое письмо";
+            message.Body = "Текст тестового письма";
+
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "n.plaksickaya",
+                //SecurePassword = "8E3ER5ereme0",
+                Password = "8E3ER5ereme0"
+            };
+
+            client.Send(message);
         }
     }
 }

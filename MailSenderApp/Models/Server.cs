c42f44db7ede9721d4626d47558c1d9889dcb21b
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-02-20 11:57:22 +0300

    Добавление моделей данных

---
+++ MailSenderApp/Models/Server.cs
@@ -0,0 +1,15 @@
+﻿namespace MailSenderApp.Models
+{
+    public class Server
+    {
+        public string Address { get; set; }
+
+        public int Port { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}

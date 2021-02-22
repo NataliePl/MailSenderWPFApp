eea3f386f2c851b1b3de154eb48208663f1e6ce0
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-02-22 22:08:36 +0300

    Редизайн тулбара в списках

--- MailSenderApp/MainWindow.xaml.cs
+++ MailSenderApp/MainWindow.xaml.cs
@@ -40,5 +40,20 @@ namespace MailSenderApp
         {
 
         }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness();
+            }
+        }
     }
 }

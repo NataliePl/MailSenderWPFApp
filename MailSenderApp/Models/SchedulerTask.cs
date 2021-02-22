dc00514a596c33d736dbd31f4243e222e70edfe3
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-02-22 13:45:25 +0300

    WPF_Меню, статус бар, таб контрол

---
+++ MailSenderApp/Models/SchedulerTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSenderApp.Models
+{
+    public class SchedulerTask
+    {
+        public DateTime Time { get; set; }
+        public Server Server { get; set; }
+        public Sender Sender { get; set; }
+        public List<Recipient> Recipients { get; set; }
+        public Message Message { get; set; }
+    }
+}
--- MailSenderApp/Models/Server.cs
+++ MailSenderApp/Models/Server.cs
@@ -2,6 +2,8 @@
 {
     public class Server
     {
+        public string Name { get; set; }
+
         public string Address { get; set; }
 
         public int Port { get; set; }

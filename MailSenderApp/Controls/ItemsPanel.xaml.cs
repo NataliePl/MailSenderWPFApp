e1d147aaaa04a059c65b47bfa7eaab747dd6f5e5
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-02-25 11:12:16 +0300

    Добавление контрол-панели

---
+++ MailSenderApp/Controls/ItemsPanel.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSenderApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ItemsPanel.xaml
+    /// </summary>
+    public partial class ItemsPanel
+    {
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(ItemsPanel),
+            new PropertyMetadata(default(string)));
+        //Свойство зависимости
+        public string Title 
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty,value);
+        }
+        public ItemsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

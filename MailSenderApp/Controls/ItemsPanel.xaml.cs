e23ae2c2b9ef349944484073d0be29d5b629c8bb
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-03-03 09:09:59 +0300

    Добавление по 4му уроку новых кнопок (edit, remove)

--- MailSenderApp/Controls/ItemsPanel.xaml.cs
+++ MailSenderApp/Controls/ItemsPanel.xaml.cs
@@ -22,22 +22,21 @@ namespace MailSenderApp.Controls
     /// </summary>
     public partial class ItemsPanel
     {
-        public static readonly DependencyProperty TitleProperty = 
+        public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register(
             nameof(Title),
             typeof(string),
             typeof(ItemsPanel),
             new PropertyMetadata(default(string)));
         //Свойство зависимости
-        public string Title 
+        public string Title
         {
             get => (string)GetValue(TitleProperty);
-            set => SetValue(TitleProperty,value);
+            set => SetValue(TitleProperty, value);
         }
         public ItemsPanel()
         {
             InitializeComponent();
-            
         }
 
 
@@ -48,9 +47,10 @@ namespace MailSenderApp.Controls
            typeof(ItemsPanel),
            new PropertyMetadata(null));
 
-        public IEnumerable ItemsList {
+        public IEnumerable ItemsList
+        {
             get => (IEnumerable)GetValue(ItemsListProperty);
-            
+
             set => SetValue(ItemsListProperty, value);
         }
 
@@ -63,7 +63,74 @@ namespace MailSenderApp.Controls
            typeof(ItemsPanel),
            new PropertyMetadata(null));
 
+
+        #region RemoveCommand
+        /// <summary> Удаление элемента </summary>
+        public static readonly DependencyProperty RemoveItemCommandProperty =
+            DependencyProperty.Register(
+                nameof(RemoveItemCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(ICommand)));
+
+        public ICommand RemoveItemCommand
+        {
+            get => (ICommand)GetValue(RemoveItemCommandProperty);
+            set => SetValue(RemoveItemCommandProperty, value);
+        }
+
+        #endregion
         
+        #region EditCommand
+        /// <summary> Удаление элемента </summary>
+        public static readonly DependencyProperty EditItemCommandProperty =
+            DependencyProperty.Register(
+                nameof(EditItemCommand),
+                typeof(ICommand),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(ICommand)));
+
+        public ICommand EditItemCommand
+        {
+            get => (ICommand)GetValue(EditItemCommandProperty);
+            set => SetValue(EditItemCommandProperty, value);
+        }
+        #endregion
+
+        #region Выбранный элемент
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItem),
+                typeof(object),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(object)));
+
+        public object SelectedItem { get => (object)GetValue(SelectedItemProperty); set => SetValue(SelectedItemProperty, value); }
+        #endregion
+
+
+        #region ItemTemplate Шаблон элемента выпадающего списка
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(
+                nameof(DataTemplate),
+                typeof(object),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(object)));
+
+        public DataTemplate ItemTemplate { get => (DataTemplate)GetValue(ItemTemplateProperty); set => SetValue(ItemTemplateProperty, value); }
+        #endregion
+
+        #region string имя отображаемого свойства
+        public static readonly DependencyProperty DisplayMemberPathProperty =
+            DependencyProperty.Register(
+                nameof(DisplayMemberPath),
+                typeof(string),
+                typeof(ItemsPanel),
+                new PropertyMetadata(default(object)));
+
+        public String DisplayMemberPath { get => (String)GetValue(DisplayMemberPathProperty); set => SetValue(DisplayMemberPathProperty, value); }
+        #endregion
+
 
         //public ItemsPanel(CommandBinding addServerCommand)
         //{

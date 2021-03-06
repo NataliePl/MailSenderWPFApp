b4cbbb69ed4830b793f815ead2d0b650c1f6bfa0
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-03-06 22:49:57 +0300

    Добавление валидации на ID получателя

--- MailSenderApp/Views/RecipientEditor.xaml.cs
+++ MailSenderApp/Views/RecipientEditor.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MailSenderApp.Views
 {
@@ -23,6 +10,30 @@ namespace MailSenderApp.Views
         public RecipientEditor()
         {
             InitializeComponent();
+        }
+
+        private void OnNameValidationError(object Sender, ValidationErrorEventArgs E)
+        {
+            if(E.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control)Sender).ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)Sender).ClearValue(ToolTipProperty);
+            }
+        }
+
+        private void OnIdValidationError(object Sender, ValidationErrorEventArgs E)
+        {
+            if (E.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control)Sender).ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)Sender).ClearValue(ToolTipProperty);
+            }
         }
     }
 }
